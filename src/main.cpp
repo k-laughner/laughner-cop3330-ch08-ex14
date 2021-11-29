6ef085e458e85ba02489cb382acce91e490f0393
k-laughner <74153467+k-laughner@users.noreply.github.com>
2021-11-29 04:08:56 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -26,10 +26,20 @@ class print {
     std::cout << "\nThe int is: " << result;
   }
 };
-
+class answerQuestions {
+  public: void answerQs() {
+    std::cout << "\nCan we declare a non-reference function argument const?\n" << "Answer: Yes, it is possible.\n\n";
+    std::cout << "What might that mean?\n" << "Answer: It means that the function's parameter cannot be changed throughout the function because it is a constant.\n\n";
+    std::cout << "Why might we want to do that?\n" << "Answer: It is a good practice to pass constants when not intending to change a value in a function because it prevents accidental changes to a value while running it through a function. If a function tries to change a value that was passed as a constant,the program will not compile. This prevents the value from being changed.\n\n";
+    std:cout << "Why don't people do it often?\n" << "Answer: In one of the major programming languages that was used for 50 years, all arguments were passed as non-constants. Due to this, it has been common practice to pass a value as not constant. In modern programming languages like C++ however, it is now best to pass constant values and avoid changing too many values in a function.\n\n";
+  }
+};
 int main() {
+  answerQuestions a;
+  a.answerQs();
   int result = 5;
-  std::cout << "Integer named result with a value of 5 is passed as a constant through a print function. A commented out line attempted to change the result to 2. Because the function is pass-by-const, attempting to change the result results in a compile error. \nThis proves how pass-by-const prevents a value from being changed in a function.\n";
+  std::cout << "\n\nDemonstration\n\n";
+  std::cout << "Integer named result with a value of 5 is passed as a constant through a print function.\nA commented out line attempted to change the result to 2. \nBecause the function is pass-by-const, attempting to change the result results in a compile error. \nThis proves how pass-by-const prevents a value from being changed in a function.\n";
   print p;
   p.printResult(result);
 } 
